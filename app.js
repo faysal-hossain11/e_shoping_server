9dec8126a5d73a72dac318a3ccc02758501657f5
faysal hossain <devfaysalbd@gamil.com>
2025-11-27 08:39:51 +0600

    some update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -40,7 +40,15 @@ async function run() {
     app.get('/products', async (req, res) => {
       const result = await productCollection.find().toArray();
       res.send(result);
-    })
+    });
+
+    // find single product 
+    app.get('/shop/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
 
     // create  product 
